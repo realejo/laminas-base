c8e10656245fa65531aaa6b30dff2fa1870a2a38
Rodrigo Capilé <rcapile@gmail.com>
2020-05-07 16:09:52 -0300

    Adicionei ExtractLogInfoTrait

---
+++ src/Utils/ExtractLogInfoTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Realejo;
+
+trait ExtractLogInfoTrait
+{
+    protected function extractLogInfo(array &$set, string $logMessage = '', string $logType = '',
+        $forceLogTypePrefix = false
+    ): array {
+        $idBlame = null;
+        if (array_key_exists('blame', $set)) {
+            $idBlame = $set['blame'];
+            unset($set['blame']);
+        }
+
+        // Verifica se há uma mensagem para o log
+        foreach (['logMessage', 'log'] as $possibleKey) {
+            if (array_key_exists($possibleKey, $set)) {
+                $logMessage = empty($set[$possibleKey]) ? $logMessage : $set[$possibleKey];
+                unset($set[$possibleKey]);
+            }
+        }
+
+        // Verifica se há uma mensagem para o log
+        foreach (['logType', 'logtipo'] as $possibleKey) {
+            if (array_key_exists($possibleKey, $set)) {
+                $logTypeCandidate = empty($set[$possibleKey]) ? $logType : $set[$possibleKey];
+                if ($forceLogTypePrefix === true && strpos($logTypeCandidate, $logType) !== 0) {
+                    $logTypeCandidate = $logType . $logTypeCandidate;
+                }
+                $logType = $logTypeCandidate;
+                unset($set[$possibleKey]);
+            }
+        }
+
+        // Remove o usuário eu está criando a promoção
+        $logData = null;
+        if (array_key_exists('logData', $set)) {
+            $logData = $set['logData'];
+            unset($set['logData']);
+        }
+
+        return [$idBlame, $logMessage, $logType, $logData];
+    }
+}
